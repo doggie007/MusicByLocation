59091f403395a408da0f5416de0d4772ad1b7781
James Tcheng <james.tcheng1234@gmail.com>
2022-02-24 16:27:19 +0000

    Initial Commit

---
+++ MusicByLocation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MusicByLocation
+//
+//  Created by Tcheng, James (BJH) on 24/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
