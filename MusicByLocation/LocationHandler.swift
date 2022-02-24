c2177c4961242866e215b4b87b557524a3da77ac
James Tcheng <james.tcheng1234@gmail.com>
2022-02-24 22:43:51 +0000

    Added GeoEncoder to get information about coordinates.

--- MusicByLocation/LocationHandler.swift
+++ MusicByLocation/LocationHandler.swift
@@ -10,6 +10,7 @@ import CoreLocation
 
 class LocationHandler: NSObject, CLLocationManagerDelegate, ObservableObject{
     let manager = CLLocationManager()
+    let geocoder = CLGeocoder()
     @Published var lastKnownLocation: String = ""
     
     override init() {
@@ -26,10 +27,25 @@ class LocationHandler: NSObject, CLLocationManagerDelegate, ObservableObject{
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let coordinates =  locations.first?.coordinate{
-            lastKnownLocation = "\(coordinates.latitude) \(coordinates.longitude)"
-        } else{
-            lastKnownLocation = "No valid location found"
+        if let firstLocation = locations.first {
+            geocoder.reverseGeocodeLocation(firstLocation, completionHandler: {(placemarks, error) in
+                if error != nil{
+                    self.lastKnownLocation = "Could not perform lookup of location from coordinate information"
+                } else{
+                    if let firstPlacemark = placemarks?[0]{
+                        
+                        let country = firstPlacemark.country ?? "Couldn't find sublocality"
+                        let city = firstPlacemark.locality ?? "Couldn't find locality"
+                        let subCity = firstPlacemark.subLocality ?? "Couldn't find sublocality"
+                        let street = firstPlacemark.thoroughfare ?? "Couldn't find sublocality"
+                        let subStreet = firstPlacemark.subThoroughfare ?? "Couldn't find sublocality"
+                        let postalCode = firstPlacemark.postalCode ?? "Couldn't find sublocality"
+                        self.lastKnownLocation = "\(country), \(city)\n \(subCity), \(street), \(subStreet)\n \(postalCode)"
+                        
+                        
+                    }
+                }
+            })
         }
     }
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
